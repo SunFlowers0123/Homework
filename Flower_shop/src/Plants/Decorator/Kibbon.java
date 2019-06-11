da0cb19988835601a0283fe9398cb9003cb96453
SunFlowers0123 <49277648+SunFlowers0123@users.noreply.github.com>
2019-06-11 15:12:36 +0300

    Update Kibbon.java

--- Flower_shop/src/Plants/Decorator/Kibbon.java
+++ Flower_shop/src/Plants/Decorator/Kibbon.java
@@ -5,21 +5,15 @@ import Plants.Product;
 public class Kibbon extends Product {
 	
 	private Product product;
-	private String name;
 	
 	public Kibbon(String nameKibbon , Product product , double priceKibbon) {
 		super(nameKibbon , priceKibbon);
 		this.product = product;
-		this.name = nameKibbon;
 	}
 
 	@Override
 	public String getName() {
 		return super.getName();
-	}
-	
-	private String getNamePaper() {
-		return name;
 	}
 	
 	@Override
@@ -35,10 +29,6 @@ public class Kibbon extends Product {
 	public String getDescription() {
 		return String.format("Name : %s Price : %.2f   %s Total price  %.2f  ",getName() , getPrice(), 
 				product.getDescription() ,getTotalPrice() );	
-	
-//		return String.format("Name : %s Price : %.2f   %s Total price  %.2f  Name goods : %s Price goods : %.2f",getName() , getPrice(), 
-//				product.getDescription() ,getTotalPrice() , getNamePaper() , super.getPrice());	
-
 	}
 
 	
